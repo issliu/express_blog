7d15e1cce8f6be8424f5b71a597608af3e7a5cea
issliu <liushun9810@gmail.com>
2017-01-23 16:37:06 +0800

    add bootstrap support

--- app.js
+++ app.js
@@ -59,7 +59,9 @@ app.use(session({
     })
 }));
 
+app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(compression());
 app.use('/', index);
 app.use('/users', users);
